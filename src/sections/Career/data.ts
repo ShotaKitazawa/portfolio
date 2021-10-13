959e763cd3020700fb8130499a68492989e47bc9
ShotaKitazawa <skitazawa1121@gmail.com>
2021-10-13 23:05:15 +0900

    update

--- src/sections/Career/data.ts
+++ src/sections/Career/data.ts
@@ -2,6 +2,7 @@ type Career = {
   period: string
   kind: string
   content: string
+  url: string
 }
 
 // TODO: github.com/ShotaKitazawa/ShotaKitazawa で管理する
@@ -10,35 +11,48 @@ export const data: Career[] = [
     period: '2021/05 ~ 2021/08',
     kind: 'イベント運営',
     content: 'ISUCON11 予選作問担当',
+    url: 'https://isucon.net/archives/55821036.html',
   },
   {
     period: '2021/02 ~',
     kind: 'イベント運営',
     content: 'CloudNative Days 実行委員 (Dreamkast 開発)',
+    url: 'https://cloudnativedays.jp/',
   },
   {
     period: '2020/06 ~ 2020/09',
     kind: 'イベント運営',
     content: 'NTT Performance Tuning Challenge 2020 運営',
+    url: 'https://www.ntt.com/index.html',
   },
   {
     period: '2019/04 ~ ',
     kind: '新卒入社',
     content: 'NTTコミュニケーションズ株式会社',
+    url: 'http://archive.interop.jp/2018/',
   },
   {
     period: '2018/06',
     kind: 'イベント運営',
     content: 'Interop Tokyo 2018 STM',
+    url: 'http://archive.interop.jp/2018/',
   },
   {
     period: '2019/03 ~ 2019/04',
     kind: 'Internship',
     content: '株式会社サイバーエージェント',
+    url: '',
   },
   {
     period: '2018/10 ~ 2018/12',
     kind: 'Internship',
     content: 'グリー株式会社',
+    url: '',
+  },
+  {
+    period: '2015/04 ~ 2019/03',
+    kind: '学生団体',
+    content: 'CIS RAT',
+    url: 'https://rat.cis.k.hosei.ac.jp/article/rat/aboutrat.html',
   },
 ]
